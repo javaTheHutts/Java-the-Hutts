abaf9761805792447ebc896de511f2a4c049abef
MarnoH <marnoh.mh@gmail.com>
2017-05-25 23:33:38 +0200

    Added a testing function for validation.

--- Testing/pytester.py
+++ Testing/pytester.py
@@ -3,3 +3,6 @@ def summation(a,b):
 
 def subtraction(a,b):
 	return summation(a,-b)
+
+def validate(name,surname,id):
+	return (name == "Marno") and (surname == "Hermann") and (id == 9512205074086)
--- Testing/test_pytest.py
+++ Testing/test_pytest.py
@@ -6,3 +6,6 @@ def test_sum():
 def test_sub():
 	subValue = p.subtraction(6,4)
 	assert subValue == 2
+def test_validate():
+	validateValue = p.validate("Marno","Hermann",9512205074086) and p.validate("Jan","Pompies",9512234081087)
+	assert validateValue == True and p.validate("","",0)==False
