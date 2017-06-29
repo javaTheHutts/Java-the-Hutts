0a39626be37d1b14c8af1c4637c6ed9dd0f6a03b
Nicolai van Niekerk <nicvaniek@gmail.com>
2017-06-29 23:40:50 +0200

    Added default values for Extraction and Validation
    Created controllers for extraction and validation that return mock data to be used in the next demo

--- src/main/python/flaskApp/api/validation/controllers.py
+++ src/main/python/flaskApp/api/validation/controllers.py
@@ -6,22 +6,27 @@ Handles all requests relevant to the validation service of the API
 ----------------------------------------------------------------------
 """
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 validate = Blueprint('validate', __name__)
 
 
-@validate.route('/validate', methods=['POST'])
+@validate.route('/validateID', methods=['POST'])
 def validateId():
     """
         ----------------------------------------------------------------------
         Author: Nicolai van Niekerk
         ----------------------------------------------------------------------
-        Sample function to return a match percentage of two received faces/images
+        Sample function to return a match percentage of an ID image and
+        provided personal information and picture of face
         ----------------------------------------------------------------------
         URL: http://localhost:5000/validate
         ----------------------------------------------------------------------
         """
+    image_of_id = request.files.get("ID")
+    name = request.form['name']
+    surname = request.form['surname']
+    face = request.files.get('face')
     result = {
-        "Percentage Match": 97
+        "Percentage Match": 63
     }
     return jsonify(result)
