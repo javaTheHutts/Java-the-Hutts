008f1ff26425f242e9f09131924046b77e7d92cd
Nicolai van Niekerk <nicvaniek@gmail.com>
2017-07-03 21:50:44 +0200

    Added Compare Section

--- Demo/js/script.js
+++ Demo/js/script.js
@@ -4,12 +4,32 @@ Author(s): Nicolai van Niekerk, Justin van Tonder
 
 /* global $ */
 $(document).ready(function() {
+
+  //Materialize component initialization
+  $('select').material_select();
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 200 // Creates a dropdown of 15 years to control year
+  });
   
   // Change caret symbol when expanding collapsibles
   $('.collapsible-header').on('click', function(){
     $(this).hasClass('active')?
-    $(this).children('.carets').text('▼'):
-    $(this).children('.carets').text('►');
+    $(this).children('.carets').text('►'):
+    $(this).children('.carets').text('▼')
   });
   
 });
+
+// Show ID Image preview
+function readURL(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('#id_image').attr('src', e.target.result);
+          };
+
+          reader.readAsDataURL(input.files[0]);
+      }
+  }
