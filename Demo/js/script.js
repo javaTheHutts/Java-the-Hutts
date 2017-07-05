707a63a1e8c7223c11d4e3e7176b92f6d5b71eae
jann_za <jvantonder0913@gmail.com>
2017-07-05 11:33:36 +0000

    Switched to card layout for expandables

--- Demo/js/script.js
+++ Demo/js/script.js
@@ -11,25 +11,25 @@ $(document).ready(function() {
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 200 // Creates a dropdown of 15 years to control year
   });
-  
+
   // Change caret symbol when expanding collapsibles
-  $('.collapsible-header').on('click', function(){
+  $('.collapsible-header').on('click', function() {
     $(this).hasClass('active')?
     $(this).children('.carets').text('►'):
-    $(this).children('.carets').text('▼')
+    $(this).children('.carets').text('▼');
   });
-  
+
 });
 
 // Show ID Image preview
 function readURL(input) {
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-          reader.onload = function (e) {
-              $('#id_image').attr('src', e.target.result);
-          };
+    reader.onload = function(e) {
+      $('#id_image').attr('src', e.target.result);
+    };
 
-          reader.readAsDataURL(input.files[0]);
-      }
+    reader.readAsDataURL(input.files[0]);
   }
+}
