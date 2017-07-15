5ceaf2470e0e9ac7bf6fc0bf552e564c86a4530c
Andreas Nel <nel.andreas1@gmail.com>
2017-07-15 18:16:14 +0200

    cleanup

--- src/main/scripts/hello-pybuilder.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3.5
-import sys
-
-sys.stdout.write('Hello from my script!\n')
-
--- src/unittest/python/test_pytest.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
-from main.python import pytester as p
-
-def test_sum():
-	sumValue = p.summation(4,5)
-	assert sumValue == 9
