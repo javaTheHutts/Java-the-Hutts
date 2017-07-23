2358517d3379d081cf6db47a0f6ec1e9dad165cb
Stephan <nellstephanj@gmail.com>
2017-07-23 10:14:15 +0200

    Images passed in the body of URL are now compatibile with CV

--- src/main/python/extraction/controllers.py
+++ src/main/python/extraction/controllers.py
@@ -1,12 +1,14 @@
 """
 ----------------------------------------------------------------------
-Author: Nicolai van Niekerk
+Author(s): Nicolai van Niekerk, Stephan Nell
 ----------------------------------------------------------------------
 Handles all requests relevant to the extraction service of the API
 ----------------------------------------------------------------------
 """
-
+from imutils.convenience import url_to_image
 from flask import Blueprint, jsonify, request
+import cv2
+import numpy as np
 
 extract = Blueprint('extract', __name__)
 
@@ -15,40 +17,74 @@ extract = Blueprint('extract', __name__)
 def extract_text():
     """
     ----------------------------------------------------------------------
-    Author: Nicolai van Niekerk
+    Author(s): Nicolai van Niekerk, Stephan Nell
     ----------------------------------------------------------------------
     Sample function to extract text from image received
     ----------------------------------------------------------------------
     URL: http://localhost:5000/extractText
     ----------------------------------------------------------------------
     """
-    image = request.files.get("image")
-    print(image)
-    text = {
-        "Surname": "Doe",
-        "Names": "John Jane",
-        "Sex": "M",
-        "Nationality": "RSA",
-        "Identity Number": "6944585228083",
-        "Date of Birth": "06-05-1996",
-        "Country of Birth": "RSA",
-        "Status": "Citizen"
-    }
-    return jsonify({"Extracted Fields": text})
+    # initialize the data dictionary to be returned by the request
+    data = {"success": False}
+
+    # check to see if this is a post request
+    if request.method == "POST":
+        # check to see if an image was uploaded
+        if request.files.get("image", None) is not None:
+            # grab the uploaded image
+            image = _grab_image(stream=request.files["image"])
+        # otherwise, assume that a URL was passed in
+        else:
+            # grab the URL from the request
+            url = request.post.get("url", None)
+            # if the URL is None, then return an error
+            if url is None:
+                data["error"] = "No URL provided."
+                return jsonify(data)
+            # load the image and convert
+            image = _grab_image(url=url)
+        # Call open CV commands here with the extracted image
+        print(image)
+        data.update({"Surname": "Doe", "Names": "John Jane", "Sex": "M", "Nationality": "RSA",
+                     "Identity Number": "6944585228083", "Date of Birth": "06-05-1996",
+                     "Country of Birth": "RSA", "Status": "Citizen", "success": True})
+    return jsonify({"Extracted Fields": data})
 
 
 @extract.route('/extractFace', methods=['POST'])
 def extract_face():
     """
     ----------------------------------------------------------------------
-    Author: Nicolai van Niekerk
+    Author(s): Nicolai van Niekerk, Stephan Nell
     ----------------------------------------------------------------------
     Sample function to extract face from image received
     ----------------------------------------------------------------------
     URL: http://localhost:5000/extractFace
     ----------------------------------------------------------------------
     """
-    image = request.files.get("image")
+    # initialize the data dictionary to be returned by the request
+    data = {"success": False}
+
+    # check to see if this is a post request
+    if request.method == "POST":
+        # check to see if an image was uploaded
+        if request.files.get("image", None) is not None:
+            # grab the uploaded image
+            image = _grab_image(stream=request.files["image"])
+
+        # otherwise, assume that a URL was passed in
+        else:
+            # grab the URL from the request
+            url = request.post.get("url", None)
+
+            # if the URL is None, then return an error
+            if url is None:
+                data["error"] = "No URL provided."
+                return jsonify(data)
+
+            # load the image and convert
+            image = _grab_image(url=url)
+    # Call open CV commands here with the extracted image
     print(image)
     face = "jklanskjcbniugciuhncoiaksc6565"
     return jsonify({"Extracted Face": face})
@@ -58,24 +94,69 @@ def extract_face():
 def extract_all():
     """
     ----------------------------------------------------------------------
-    Author: Nicolai van Niekerk
+    Author(s): Nicolai van Niekerk, Stephan Nell
     ----------------------------------------------------------------------
     Sample function to extract face and text from image received
     ----------------------------------------------------------------------
     URL: http://localhost:5000/extractAll
     ----------------------------------------------------------------------
     """
-    image = request.files.get("image")
-    print(image)
-    data = {
-        "Surname": "Doe",
-        "Names": "John Jane",
-        "Sex": "M",
-        "Nationality": "RSA",
-        "Identity Number": "6944585228083",
-        "Date of Birth": "06-05-1996",
-        "Country of Birth": "RSA",
-        "Status": "Citizen",
-        "Face": "jklanskjcbniugciuhncoiaksc6565"
-    }
-    return jsonify({"Extracted data": data})
+    # initialize the data dictionary to be returned by the request
+    data = {"success": False}
+    # check to see if this is a post request
+    if request.method == "POST":
+        # check to see if an image was uploaded
+        if request.files.get("image", None) is not None:
+            # grab the uploaded image
+            image = _grab_image(stream=request.files["image"])
+
+        # otherwise, assume that a URL was passed in
+        else:
+            # grab the URL from the request
+            url = request.post.get("url", None)
+
+            # if the URL is None, then return an error
+            if url is None:
+                data["error"] = "No URL provided."
+                return jsonify(data)
+
+            # load the image and convert
+            image = _grab_image(url=url)
+        # Call open CV commands here with the extracted image
+        print(image)
+        data.update({"Surname": "Nell", "Names": "Stephan Jack", "Sex": "M", "Nationality": "RSA",
+                     "Identity Number": "9511068172098", "Date of Birth": "06-11-1995",
+                     "Country of Birth": "RSA", "Status": "Citizen", "face": "McFace", "success": True})
+    return jsonify({"Extracted Fields": data})
+
+
+def _grab_image(path=None, stream=None, url=None):
+    """
+    This function grabs the image from URL, or image path and applies necessary changes to the grabbed
+    images so that the image is compatible with OpenCV operation.
+    Author(s):
+        Stephan Nell
+    Args:
+        path (str): The path to the image if it reside on disk
+        stream (str): A stream of text representing where on the internet the image resides
+        url(str): Url representing a path to where an image should be fetched.
+    Returns:
+        (:obj:'OpenCV image'): Image that is now compatible with OpenCV operations
+    TODO:
+        Return a Json error indicating file not found
+    """
+    # If the path is not None, then load the image from disk. Example: payload = {"image": open("id.jpg", "rb")}
+    if path is not None:
+        image = cv2.imread(path)
+    # otherwise, the image does not reside on disk
+    else:
+        # if the URL is not None, then download the image
+        if url is not None:
+            return url_to_image(url)
+        # if the stream is not None, then the image has been uploaded
+        elif stream is not None:
+            # Example: "http://www.pyimagesearch.com/wp-content/uploads/2015/05/obama.jpg"
+            data = stream.read()
+            image = np.asarray(bytearray(data), dtype="uint8")
+            image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
