a6bd71b873aea7272e0aada993b7cbd236a0499b
jan-justin <u15073298@tuks.co.za>
2017-07-27 11:52:52 +0000

    Updated results modal and hid initially unneccessary content.

--- demo/js/script.js
+++ demo/js/script.js
@@ -5,12 +5,15 @@ Author(s): Nicolai van Niekerk, Justin van Tonder
 /* global $ */
 $(document).ready(function() {
 
+  // Hide some content
+  $('.initially-hidden').hide();
+
   // Initialize slide menu buttons
   $('.slide-btn').sideNav({
     menuWidth: 300,
     closeOnClick: true
   });
-  
+
   // Hide existing side navs
   $('.slide-btn').on('click', function() {
     // Not the best fix... but workable
@@ -18,20 +21,20 @@ $(document).ready(function() {
       $('#sidenav-overlay').trigger('click');
     }
   });
-  
+
   // Initialise all modals
   $('.modal').modal();
 
   // Materialise component initialization
   $('select').material_select();
-  
+
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 110, // Creates a dropdown of 15 years to control year,
     format: 'dd/mm/yyyy',
     closeOnSelect: false // Close upon selecting a date,
   });
-  
+
   // Image sliders initialisation
   $('.pipelet').slick({
     slidesToShow: 1,
@@ -40,7 +43,7 @@ $(document).ready(function() {
     fade: true,
     asNavFor: '.pipeline'
   });
-  
+
   $('.pipeline').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -77,75 +80,24 @@ $(document).ready(function() {
       });
     }
   });
-  
+
   // Click event to scroll to top
 	$('.scroll-top').on('click', function() {
 		$('html, body').animate({scrollTop: 0}, 800);
 		return false;
 	});
-	
+
 	// Click event to scroll to bottom
 	$('.scroll-bottom').on('click', function() {
 		$('html, body').animate({scrollTop: $('body').height()}, 800);
 		return false;
 	});
-  
+
   // Result modal initialisation
-  $('#verify-result').modal({
-    endingTop: '22%',
-    ready: function(modal, trigger) {
-      console.log($('#result').data('percentage'));
-      // Results circliful
-      $('#result-total').circliful({
-        percent: $('#result-total').data('percentage'),
-        text: 'Total',
-        textBelow: true,
-        decimals: 2,
-        alwaysDecimals: true,
-        foregroundColor: '#80cbc4',
-        backgroundColor: 'none',
-        fillColor: '#eee',
-        foregroundBorderWidth: 4,
-        iconColor: '#80cbc4',
-        icon: 'f2c3',
-        iconSize: '30',
-        iconPosition: 'middle'
-      });
-      
-      $('#result-text').circliful({
-        percent: $('#result-text').data('percentage'),
-        text: 'Text',
-        textBelow: true,
-        decimals: 2,
-        alwaysDecimals: true,
-        foregroundColor: '#80cbc4',
-        backgroundColor: 'none',
-        fillColor: '#eee',
-        foregroundBorderWidth: 4,
-        iconColor: '#80cbc4',
-        icon: 'f022',
-        iconSize: '30',
-        iconPosition: 'middle'
-      });
-      
-      $('#result-profile').circliful({
-        percent: $('#result-profile').data('percentage'),
-        text: 'Profile',
-        textBelow: true,
-        decimals: 2,
-        alwaysDecimals: true,
-        foregroundColor: '#80cbc4',
-        backgroundColor: 'none',
-        fillColor: '#eee',
-        foregroundBorderWidth: 4,
-        iconColor: '#80cbc4',
-        icon: 'f007',
-        iconSize: '30',
-        iconPosition: 'middle'
-      });
-    }
+  $('.verify-result').modal({
+    endingTop: '20%',
   });
-  
+
   // Modal trigger
   $('#verify-btn').on('click', function(e) {
     e.preventDefault();
@@ -173,26 +125,101 @@ $(document).ready(function() {
     formData.append('dob', dob);
 
     $.ajax({
-        type: "POST",
-        url: "http://localhost:5000/verifyID",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function(data){
-            alert("Match: " + data.percent_match + "%");
-        }
+      type: "POST",
+      url: "http://localhost:5000/verifyID",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(data){
+        $('.circle-result').html('');
+        $('.result-total')[0].data('percentage', data.total_match);
+        $('.result-text')[0].data('percentage', data.total_match);
+        $('.result-profile')[0].data('percentage', data.total_match);
+
+        // Results circliful
+        $('.result-total').circliful({
+          percent: $('.result-total')[0].data('percentage'),
+          text: 'Total',
+          textBelow: true,
+          decimals: 2,
+          alwaysDecimals: true,
+          foregroundColor: '#80cbc4',
+          backgroundColor: 'none',
+          fillColor: '#eee',
+          foregroundBorderWidth: 4,
+          iconColor: '#80cbc4',
+          icon: 'f2c3',
+          iconSize: '30',
+          iconPosition: 'middle'
+        });
+
+        $('.result-text').circliful({
+          percent: $('.result-text')[0].data('percentage'),
+          text: 'Text',
+          textBelow: true,
+          decimals: 2,
+          alwaysDecimals: true,
+          foregroundColor: '#80cbc4',
+          backgroundColor: 'none',
+          fillColor: '#eee',
+          foregroundBorderWidth: 4,
+          iconColor: '#80cbc4',
+          icon: 'f022',
+          iconSize: '30',
+          iconPosition: 'middle'
+        });
+
+        $('.result-profile').circliful({
+          percent: $('.result-profile')[0].data('percentage'),
+          text: 'Profile',
+          textBelow: true,
+          decimals: 2,
+          alwaysDecimals: true,
+          foregroundColor: '#80cbc4',
+          backgroundColor: 'none',
+          fillColor: '#eee',
+          foregroundBorderWidth: 4,
+          iconColor: '#80cbc4',
+          icon: 'f007',
+          iconSize: '30',
+          iconPosition: 'middle'
+        });
+
+        $('.spinner').hide();
+        $('.circle-results-wrapper, #verify-result.modal .modal-footer')
+          .show(500);
+      }
     });
-    
-    // Move up once working
-    $('#result-total').html('');
-    $('#result-total').data('percentage', 86.55);
-    $('#result-text').html('');
-    $('#result-text').data('percentage', 84);
-    $('#result-profile').html('');
-    $('#result-profile').data('percentage', 88.99);
+
+    // Testing -- Remove
+    $('.test-circle').circliful({
+      percent: 88.8,
+      text: 'Profile',
+      textBelow: true,
+      decimals: 2,
+      alwaysDecimals: true,
+      foregroundColor: '#80cbc4',
+      backgroundColor: 'none',
+      fillColor: '#eee',
+      foregroundBorderWidth: 4,
+      iconColor: '#80cbc4',
+      icon: 'f007',
+      iconSize: '30',
+      iconPosition: 'middle'
+    });
+
+    // Open up result modal
+    $('#verify-result.modal .modal-footer').hide();
     $('#verify-result').modal('open');
   });
-  
+
+  // View detailed results click
+  $('#verify-result.modal .modal-footer').on('click', function() {
+    $('.collapsible-main').collapsible('close', 1);
+    $('.collapsible-main').collapsible('open', 4);
+    $('body').scrollTo($('#detailed-results > .collapsiblebody'), 1000);
+  });
+
   // Hover compare cards on compare button hover
   $('#verify-btn, .extraction-options button').hover(function() {
     $('.duo-card').addClass('duo-card-hover');
@@ -201,31 +228,30 @@ $(document).ready(function() {
   });
 
   // Extract text
- $('#extract-text-btn').on('click', function(e) {
-    e.preventDefault();
-    var formData = new FormData();
-    var idPhoto = document.getElementById('id-photo-extract').files[0];
-    formData.append('idPhoto', idPhoto);
+   $('#extract-text-btn').on('click', function(e) {
+      e.preventDefault();
+      var formData = new FormData();
+      var idPhoto = document.getElementById('id-photo-extract').files[0];
+      formData.append('idPhoto', idPhoto);
 
-    $.ajax({
+      $.ajax({
         type: "POST",
         url: "http://localhost:5000/extractText",
         data: formData,
         processData: false,
         contentType: false,
-        success: function(data){
+        success: function(data) {
           $("input[id$=extract]").each(function(){
             var id = $(this).attr("id").replace("-extract", "");
-            if(id != "id-photo")
-            {
+            if(id != "id-photo") {
               $(this).focus();
               $(this).val(data[id]);
               $(this).blur();
             }
           });
         }
+      });
     });
-  });
 
   // Extract profile
   $('#extract-photo-btn').on('click', function(e){
@@ -246,7 +272,7 @@ $(document).ready(function() {
   });
 
   // Extract all
-  $('#extract-all-btn').on('click', function(e){
+  $('#extract-all-btn').on('click', function(e) {
     var formData = new FormData();
     var idPhoto = document.getElementById('id-photo-extract').files[0];
     formData.append('idPhoto', idPhoto);
@@ -258,24 +284,22 @@ $(document).ready(function() {
         processData: false,
         contentType: false,
         success: function(data){
-
           // Populate text fields
-          $("input[id$=extract]").each(function(){
+          $("input[id$=extract]").each(function() {
             var id = $(this).attr("id").replace("-extract", "");
-            if(id != "id-photo")
-            {
+            if(id != "id-photo") {
               $(this).focus();
               $(this).val(data[id]);
               $(this).blur();
             }
           });
-          
-          // Show face 
+
+          // Show face
           $('#face-preview-extract').attr('src', data.face);
         }
     });
   });
-  
+
 });
 
 // Show ID Image preview
