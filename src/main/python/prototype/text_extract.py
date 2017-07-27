75b560a69eeef099f1fb542671209d43fe306dd0
MarnoH <marnoh.mh@gmail.com>
2017-07-27 16:13:22 +0200

    Functionized text extracteor to give information to server. Added birth date to text manager.

--- src/main/python/prototype/text_extract.py
+++ src/main/python/prototype/text_extract.py
@@ -30,93 +30,83 @@ import json
 SHAPE_PREDICTOR_PATH = "{base_path}/trained_data/shape_predictor_face_landmarks.dat".format(
     base_path=os.path.abspath(os.path.dirname(__file__)))
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="path to input image to be OCR'd")
-ap.add_argument("-t", "--thresholding", type=str, default=None,
-                help="type of thresholding technique")
-ap.add_argument("-b", "--blur", type=str, default="gaussian",
-                help="Blur image technique")
-ap.add_argument("-c", "--color", type=str, default=None,
-                help="Remove color channel")
-ap.add_argument("-r", "--remove", type=bool, default=None,
-                help="Remove Face")
-ap.add_argument("-k", "--kernel", default=None, nargs='+', type=int,
-                help="Kernel size for selected blur")
-args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
+class TextExtractor:
+    def extract(img, thresh="adaptive", blurr="median", clr="red", rm=False, knl=[7]):
 
-simplification_manager = SimplificationManager()
-barcode_manager = BarCodeManager()
-color_manager = ColorManager()
-face_detector = FaceDetector(SHAPE_PREDICTOR_PATH)
-image = simplification_manager.perspectiveTransformation(image)
-cv2.imwrite("output/3-warped.png", image)
+        image = cv2.imread(img)
 
+        simplification_manager = SimplificationManager()
+        barcode_manager = BarCodeManager()
+        color_manager = ColorManager()
+        face_detector = FaceDetector(SHAPE_PREDICTOR_PATH)
+        image = simplification_manager.perspectiveTransformation(image)
+        cv2.imwrite("output/3-warped.png", image)
 
-barcode_data_found, barcode_scan_data, image = barcode_manager.get_barcode_info(image)
-if barcode_data_found:
-    data = {
-       'ID_number': barcode_scan_data.decode('utf-8'),
-    }
-    card_data = json.dumps(data)
-    print(card_data)
+        barcode_data_found, barcode_scan_data, image = barcode_manager.get_barcode_info(image)
+        if barcode_data_found:
+            data = {
+                'ID_number': barcode_scan_data.decode('utf-8'),
+            }
+            card_data = json.dumps(data)
+            print(card_data)
 
-if args["remove"] is True:
-    image = face_detector.blur_face(image)
-    cv2.imwrite("output/4-faceRemvoal.png", image)
+        if rm is True:
+            image = face_detector.blur_face(image)
+            cv2.imwrite("output/4-faceRemvoal.png", image)
 
-if args["color"] is not None:
-    if args["color"] == "blackhat":
-        image = color_manager.blackHat(image)
-    elif args["color"] == "tophat":
-        image = color_manager.topHat(image)
-    else:
-        image = color_manager.extractChannel(image, args["color"])
-    cv2.imwrite("output/5-colour_extract.png", image)
+        if clr is not None:
+            if clr == "blackhat":
+                image = color_manager.blackHat(image)
+            elif clr == "tophat":
+                image = color_manager.topHat(image)
+            else:
+                image = color_manager.extractChannel(image, clr)
+            cv2.imwrite("output/5-colour_extract.png", image)
 
-cv2.imwrite("output/colour_extract.png", image)
-if args["kernel"] is not None:
-    blur_kernel = args["kernel"]
-else:
-    if args["blur"] == "median":
-        blur_kernel = [3]
-    else:
-        blur_kernel = [(3, 3)]
+        cv2.imwrite("output/colour_extract.png", image)
+        if knl is not None:
+            blur_kernel = knl
+        else:
+            if blurr == "median":
+                blur_kernel = [3]
+            else:
+                blur_kernel = [(3, 3)]
 
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("output/6-gray.png", image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("output/6-gray.png", image)
 
-if args["blur"] is not None:
-    blur_manager = BlurManager()
-    if args["blur"] == "blur":
-        image = blur_manager.blur(image, blur_kernel=blur_kernel)
-    elif args["blur"] == "gaussian":
-        image = blur_manager.gaussianBlur(image, blur_kernel=blur_kernel)
-    elif args["blur"] == "median":
-        image = blur_manager.medianBlur(image, blur_kernel=blur_kernel)
+        if blurr is not None:
+            blur_manager = BlurManager()
+            if blurr == "blur":
+                image = blur_manager.blur(image, blur_kernel=blur_kernel)
+            elif blurr == "gaussian":
+                image = blur_manager.gaussianBlur(image, blur_kernel=blur_kernel)
+            elif blurr == "median":
+                image = blur_manager.medianBlur(image, blur_kernel=blur_kernel)
 
-cv2.imwrite("output/7-blur.png", image)
-if args["thresholding"] is not None:
-    thresh_manager = ThresholdingManager()
-    if args["thresholding"] == "adaptive":
-        image = thresh_manager.adaptiveThresholding(image)
-    elif args["thresholding"] == "otsu":
-        image = thresh_manager.otsuThresholding(image)
+        cv2.imwrite("output/7-blur.png", image)
+        if thresh is not None:
+            thresh_manager = ThresholdingManager()
+            if thresh == "adaptive":
+                image = thresh_manager.adaptiveThresholding(image)
+            elif thresh == "otsu":
+                image = thresh_manager.otsuThresholding(image)
 
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, image)
+        filename = "{}.png".format(os.getpid())
+        cv2.imwrite(filename, image)
 
-cv2.imwrite("output/8-Extraction.png", image)
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
+        cv2.imwrite("output/8-Extraction.png", image)
+        text = pytesseract.image_to_string(Image.open(filename))
+        os.remove(filename)
 
-# Text cleanup and retrieval
-text_manager = TextManager()
-print(text, "\n------------------------------------------------------")
-clean_text = text_manager.clean_up(text)
-print(clean_text, "\n -----------------------------------------------")
-id_details = text_manager.dictify(clean_text)
-print(id_details)
+        # Text cleanup and retrieval
+        text_manager = TextManager()
+        print(text, "\n------------------------------------------------------")
+        clean_text = text_manager.clean_up(text)
+        print(clean_text, "\n -----------------------------------------------")
+        id_details = text_manager.dictify(clean_text)
+        print(id_details)
+        return id_details
+
+    #extract("img/id_card.jpg")
