68ea2dd49e34128a727cfa98f6fb4fcb83627cda
Jan-Justin van Tonder <u15073298@tuks.co.za>
2017-08-21 11:30:35 +0200

    Changed lamda expressions into static methods for readibality.

--- src/main/python/verification/text_verify.py
+++ src/main/python/verification/text_verify.py
@@ -1,12 +1,33 @@
+"""
+----------------------------------------------------------------------
+Authors: Jan-Justin van Tonder
+----------------------------------------------------------------------
+Contains the logic used to verify the extracted text from a form
+of ID.
+----------------------------------------------------------------------
+"""
+
 import Levenshtein
 from server.hutts_logger import logger, prettify_json_message
 
 
 class TextVerify:
+    """
+    This class is responsible for the verification of text that is extracted from an ID
+    and is passed in along with information that is to be used to verify the extracted
+    text.
+
+    Authors:
+        Jan-Justin van Tonder
+    """
     def __init__(self):
+        """
+        Initialises the TextVerify object.
+
+        Authors:
+            Jan-Justin van Tonder
+        """
         logger.debug('Initialising TextVerify...')
-        self.percentage_match = lambda str_x, str_y: Levenshtein.ratio(str_x, str_y) * 100
-        self.total_match = lambda matches: sum(val for val in matches.values()) / len(matches)
 
     def verify(self, extracted, verifier, threshold=0.75, min_matches=3, verbose=False):
         min_percentage = threshold * 100
@@ -27,15 +48,18 @@ class TextVerify:
         for key, value in verifier.items():
             if key in extracted and extracted[key]:
                 logger.debug('Computing match "' + value + '" and "' + extracted[key] + '"...')
-                scores[key] = self.percentage_match(value, extracted[key])
+                scores[key] = self._percentage_match(value, extracted[key])
                 logger.debug('"' + value + '" and "' + extracted[key] + '" match percentage is : ' + str(scores[key]))
             else:
                 logger.warning('Could not find corresponding field "' + key + '" in extracted information to verify')
-        # Calculate the total match score.
+        # Determine the number of scores calculated and initialise a default value for the total match score.
         num_scores = len(scores)
         total_score = 0.0
+        # Check if enough matches were found.
         if num_scores >= min_matches and num_scores > 0:
-            total_score = self.total_match(scores)
+            # Calculate the total match score.
+            total_score = self._total_match(scores)
+        # Either the minimum number of scores criteria was not met, or their were no matches at all.
         else:
             logger.warning('A total of ' + str(num_scores) + ' was found, which is less than the minimum')
         # Return the final result
@@ -43,3 +67,11 @@ class TextVerify:
             return total_score >= min_percentage, total_score
         scores['total'] = total_score
         return total_score >= min_percentage, scores
+
+    @staticmethod
+    def _percentage_match(str_x, str_y):
+        return Levenshtein.ratio(str_x, str_y) * 100
+
+    @staticmethod
+    def _total_match(matches):
+        return sum(matches.values()) / len(matches)
