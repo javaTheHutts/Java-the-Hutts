6a484beb840741e643718f776ee143f74af977bb
Jan-Justin van Tonder <u15073298@tuks.co.za>
2017-08-22 20:09:59 +0200

    Renamed some variables and functions for clarity.

--- src/main/python/verification/text_verify.py
+++ src/main/python/verification/text_verify.py
@@ -65,7 +65,7 @@ class TextVerify:
         # Logging for debugging and verbose purposes.
         logger.debug('Threshold for verification set as: ' + str(min_percentage))
         logger.debug('Minimum number of matches for verification set as: ' + str(min_matches))
-        logger.debug('Simplified scores to be returned' if not verbose else 'Verbose scores to be returned')
+        logger.debug('Simplified percentages to be returned' if not verbose else 'Verbose percentages to be returned')
         logger.debug('Verifying:')
         # Prettify and log the extracted information.
         [logger.debug(log_line) for log_line in prettify_json_message(extracted).split('\n')]
@@ -73,43 +73,44 @@ class TextVerify:
         # Prettify and log the verifier information.
         [logger.debug(log_line) for log_line in prettify_json_message(verifier).split('\n')]
         # Initialise a dictionary to house the final matching percentages.
-        scores = {}
+        match_percentages = {}
         # Iterate over the verifier and calculate a percentage match for the values,
         # if the keys match and the corresponding values exist.
         for key, value in verifier.items():
             if key in extracted and extracted[key]:
                 logger.debug('Computing match "' + str(value) + '" and "' + str(extracted[key]) + '"...')
-                scores[key] = self._percentage_match(value, extracted[key])
-                logger.debug('"' + value + '" and "' + extracted[key] + '" match percentage is : ' + str(scores[key]))
+                match_percentages[key] = self._match_percentage(value, extracted[key])
+                logger.debug('"' + value + '" and "' + extracted[key] + '" match percentage is : ' +
+                             str(match_percentages[key]))
             else:
                 logger.warning('Could not find corresponding field "' + key + '" in extracted information to verify')
-        # Determine the number of scores calculated and initialise a default value for the total match score.
-        num_scores = len(scores)
-        total_score = 0.0
+        # Determine the number of percentages calculated and initialise a default value for the total match score.
+        num_scores = len(match_percentages)
+        total_match_percentage = 0.0
         # Check if enough matches were found.
         if num_scores >= min_matches:
             # Calculate the total match score.
-            total_score = self._total_match(scores)
-        # Either the minimum number of scores criteria was not met.
+            total_match_percentage = self._total_percentage_match(match_percentages)
+        # Either the minimum number of percentages criteria was not met.
         else:
             logger.warning('A total of ' + str(num_scores) + ' matches were found, which is less than the minimum')
         # Determine whether or not the text is verified.
-        is_verified = total_score >= min_percentage
+        is_verified = total_match_percentage >= min_percentage
         # Logging for debugging purposes.
         logger.debug('Intermediate match percentages:')
-        [logger.debug(log_line) for log_line in prettify_json_message(scores).split('\n')]
-        logger.debug('Final match percentage: ' + str(total_score))
+        [logger.debug(log_line) for log_line in prettify_json_message(match_percentages).split('\n')]
+        logger.debug('Final match percentage: ' + str(total_match_percentage))
         logger.debug('Threshold to pass: ' + str(min_percentage))
         logger.debug('Result' + 'Passed' if is_verified else 'Failed')
         # Return the final result.
         if not verbose:
-            return is_verified, total_score
-        # Append the total to the existing scores for verbose, and return all percentage values.
-        scores['total'] = total_score
-        return is_verified, scores
+            return is_verified, total_match_percentage
+        # Append the total to the existing percentages for verbose, and return all percentage values.
+        match_percentages['total'] = total_match_percentage
+        return is_verified, match_percentages
 
     @staticmethod
-    def _percentage_match(str_x, str_y):
+    def _match_percentage(str_x, str_y):
         """
         This function is responsible for determining the percentage match for two strings and returning
         said percentage.
@@ -135,7 +136,7 @@ class TextVerify:
         return Levenshtein.ratio(str_x, str_y) * 100
 
     @staticmethod
-    def _total_match(matches):
+    def _total_percentage_match(matches):
         """
         This function is responsible for calculating a single, total percentage match value for a dict of match
         values that have been calculated.
