5d71d84a5fe4cfbc0707fafb3accf285c69b8a0a
Jan-Justin van Tonder <u15073298@tuks.co.za>
2017-09-06 10:32:04 +0200

    Changed string formatting for logs and exceptions.

--- src/main/python/verification/text_verify.py
+++ src/main/python/verification/text_verify.py
@@ -18,6 +18,7 @@ class TextVerify:
     Authors:
         Jan-Justin van Tonder
     """
+
     def __init__(self):
         """
         Initialises the TextVerify object.
@@ -50,40 +51,59 @@ class TextVerify:
                 values if verbose is True.
         """
         if type(extracted) is not dict:
-            raise TypeError('Bad type for arg extracted - expected dict. Received type ' + str(type(extracted)))
+            raise TypeError(
+                'Bad type for arg extracted - expected dict. Received type "%s"' %
+                type(extracted).__name__
+            )
         if type(verifier) is not dict:
-            raise TypeError('Bad type for arg verifier - expected dict. Received type ' + str(type(verifier)))
+            raise TypeError(
+                'Bad type for arg verifier - expected dict. Received type "%s"' %
+                type(verifier).__name__
+            )
         if type(threshold) is not float:
-            raise TypeError('Bad type for arg threshold - expected float. Received type ' + str(type(threshold)))
+            raise TypeError(
+                'Bad type for arg threshold - expected float. Received type "%s"' %
+                type(threshold).__name__
+            )
         if type(min_matches) is not int:
-            raise TypeError('Bad type for arg min_matches - expected int. Received type ' + str(type(min_matches)))
+            raise TypeError(
+                'Bad type for arg min_matches - expected int. Received type "%s"' %
+                type(min_matches).__name__
+            )
         if type(verbose) is not bool:
-            raise TypeError('Bad type for arg verbose - expected bool. Received type ' + str(type(verbose)))
+            raise TypeError(
+                'Bad type for arg verbose - expected bool. Received type "%s"' %
+                type(verbose).__name__
+            )
         # Set minimum number of matches, if zero or less set to one.
         min_matches = min_matches if min_matches > 0 else 1
         min_percentage = threshold * 100
         # Logging for debugging and verbose purposes.
-        logger.debug('Threshold for verification set as: ' + str(min_percentage))
-        logger.debug('Minimum number of matches for verification set as: ' + str(min_matches))
+        logger.debug('Threshold for verification set as: %.2f' % min_percentage)
+        logger.debug('Minimum number of matches for verification set as: %d' % min_matches)
         logger.debug('Simplified percentages to be returned' if not verbose else 'Verbose percentages to be returned')
+        logger.debug('-' * 50)
         logger.debug('Verifying:')
-        # # Prettify and log the extracted information.
+        logger.debug('-' * 50)
+        # Prettify and log the extracted information.
         [logger.debug(log_line) for log_line in prettify_json_message(extracted).split('\n')]
+        logger.debug('-' * 50)
         logger.debug('Against:')
-        # # Prettify and log the verifier information.
+        logger.debug('-' * 50)
+        # Prettify and log the verifier information.
         [logger.debug(log_line) for log_line in prettify_json_message(verifier).split('\n')]
+        logger.debug('-' * 50)
         # Initialise a dictionary to house the final matching percentages.
         match_percentages = {}
         # Iterate over the verifier and calculate a percentage match for the values,
         # if the keys match and the corresponding values exist.
         for key, value in verifier.items():
             if key in extracted and extracted[key]:
-                # logger.debug('Computing match "' + str(value) + '" and "' + str(extracted[key]) + '"...')
+                logger.debug('Computing match "%s" and "%s"...' % (value, extracted[key]))
                 match_percentages[key] = self._match_percentage(value, extracted[key])
-                # logger.debug('"' + value + '" and "' + extracted[key] + '" match percentage is : ' +
-                # str(match_percentages[key]))
-            # else:
-                # logger.warning('Could not find corresponding field "' + key + '" in extracted information to verify')
+                logger.debug('"%s" and "%s" match percentage: %.2f' % (value, extracted[key], match_percentages[key]))
+            else:
+                logger.warning('Could not find corresponding field "%s" in extracted information to verify' % key)
         # Determine the number of percentages calculated and initialise a default value for the total match score.
         num_scores = len(match_percentages)
         total_match_percentage = 0.0
@@ -92,16 +112,19 @@ class TextVerify:
             # Calculate the total match score.
             total_match_percentage = self._total_percentage_match(match_percentages)
         # Either the minimum number of percentages criteria was not met.
-        # else:
-            # logger.warning('A total of ' + str(num_scores) + ' matches were found, which is less than the minimum')
+        else:
+            logger.warning('A total of %d matches were found, which is less than the minimum' % num_scores)
         # Determine whether or not the text is verified.
         is_verified = total_match_percentage >= min_percentage
         # Logging for debugging purposes.
-        # logger.debug('Intermediate match percentages:')
-        # [logger.debug(log_line) for log_line in prettify_json_message(match_percentages).split('\n')]
-        # logger.debug('Final match percentage: ' + str(total_match_percentage))
-        # logger.debug('Threshold to pass: ' + str(min_percentage))
-        # logger.debug('Result: ' + 'Passed' if is_verified else 'Failed')
+        logger.debug('-' * 50)
+        logger.debug('Intermediate match percentages:')
+        logger.debug('-' * 50)
+        [logger.debug(log_line) for log_line in prettify_json_message(match_percentages).split('\n')]
+        logger.debug('-' * 50)
+        logger.debug('Final match percentage: %.2f' % total_match_percentage)
+        logger.debug('Threshold to pass: %.2f' % min_percentage)
+        logger.debug('Result: ' + 'Passed' if is_verified else 'Failed')
         # Return the final result.
         if not verbose:
             return is_verified, total_match_percentage
@@ -130,9 +153,15 @@ class TextVerify:
             TypeError: If str_y is not a string.
         """
         if type(str_x) is not str:
-            raise TypeError('Bad type for arg str_x - expected string. Received type ' + str(type(str_x)))
+            raise TypeError(
+                'Bad type for arg str_x - expected string. Received type "%s"' %
+                type(str_x).__name__
+            )
         if type(str_y) is not str:
-            raise TypeError('Bad type for arg str_y - expected string. Received type ' + str(type(str_y)))
+            raise TypeError(
+                'Bad type for arg str_y - expected string. Received type "%s"' %
+                type(str_y).__name__
+            )
         return round(Levenshtein.ratio(str_x, str_y) * 100, 2)
 
     @staticmethod
