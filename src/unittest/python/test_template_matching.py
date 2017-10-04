4977c90c258e761aba312c86c8759f21a7d33df4
nellstephanj@gmail.com <nellstephanj@gmail.com>
2017-10-04 18:09:21 +0200

    Fixed as Requested Descriptive naming

--- src/unittest/python/test_template_matching.py
+++ src/unittest/python/test_template_matching.py
@@ -17,46 +17,52 @@ student_card = cv2.imread(TEMPLATE_DIR + "pp2.jpg")
 id_book = cv2.imread(TEMPLATE_DIR + "temp_flag.jpg")
 brick = cv2.imread(TEMPLATE_DIR + "bricks.png")
 
-r = 900.0/brick.shape[1]
-dim = (900,int(brick.shape[0]*r))
+dimension_ratio = 900.0 / brick.shape[1]
+dim = (900, int(brick.shape[0] * dimension_ratio))
 no_match = cv2.resize(brick, dim, interpolation=cv2.INTER_AREA)
 
-dim = (1000,200)
+dim = (1000, 200)
 wrong_dim = cv2.resize(brick, dim, interpolation=cv2.INTER_AREA)
 
 direct_match = cv2.imread(TEMPLATE_DIR + "ID.jpg")
+
 
 def test_wrong_param():
     template = TemplateMatching()
     with pytest.raises(TypeError):
         assert template.identify(21)
 
+
 def test_match_template():
     template = TemplateMatching()
     cv2.setNumThreads(0)
-    assert template.identify(direct_match) == 'idcard'
+    assert template.identify(direct_match) is 'idcard'
     cv2.setNumThreads(-1)
+
 
 def test_wrong_dim_template():
     template = TemplateMatching()
     cv2.setNumThreads(0)
-    assert template.identify(wrong_dim) == 'None'
+    assert template.identify(wrong_dim) is 'None'
     cv2.setNumThreads(-1)
+
 
 def test_no_match_template():
     template = TemplateMatching()
     cv2.setNumThreads(0)
-    assert template.identify(no_match) == 'None'
+    assert template.identify(no_match) is 'None'
     cv2.setNumThreads(-1)
+
 
 def test_student_template():
     template = TemplateMatching()
     cv2.setNumThreads(0)
-    assert template.identify(student_card) == 'studentcard'
+    assert template.identify(student_card) is 'studentcard'
     cv2.setNumThreads(-1)
+
 
 def test_to_small_template():
     template = TemplateMatching()
     cv2.setNumThreads(0)
-    assert template.identify(id_book) == 'None'
+    assert template.identify(id_book) is 'None'
     cv2.setNumThreads(-1)
