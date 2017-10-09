1597b482ad3d221cfcb0e95e8857a5701962754d
Jan-Justin van Tonder <u15073298@tuks.co.za>
2017-10-09 12:06:20 +0200

    Preparing branch for demo interface migration.

--- Testing/pytester.py
+++
@@ -1,8 +0,0 @@
-def summation(a,b):
-	return a+b
-
-def subtraction(a,b):
-	return summation(a,-b)
-
-def validate(name,surname,id):
-	return (name == "Marno") and (surname == "Hermann") and (id == 9512205074086)
