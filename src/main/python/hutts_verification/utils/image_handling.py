116a9b7bb15db5c583c748829742567950a1cca3
Andreas Nel <nel.andreas1@gmail.com>
2017-10-17 13:57:17 +0200

    communication changed from formdata to JSON

--- src/main/python/hutts_verification/utils/image_handling.py
+++ src/main/python/hutts_verification/utils/image_handling.py
@@ -4,10 +4,11 @@ Utility functions to manage image handling from given parameters.
 
 import cv2
 import numpy as np
+import base64
 from imutils.convenience import url_to_image
 from hutts_verification.utils.hutts_logger import logger
 
-__author__ = "Stephan Nell"
+__authors__ = "Stephan Nell, Andreas Nel"
 __copyright__ = "Copyright 2017, Java the Hutts"
 __license__ = "BSD"
 __maintainer__ = "Stephan Nell"
@@ -15,15 +16,16 @@ __email__ = "nellstephanj@gmail.com"
 __status__ = "Development"
 
 
-def grab_image(path=None, stream=None, url=None):
+def grab_image(path=None, stream=None, url=None, string=None):
     """
     This function grabs the image from URL, or image path and applies necessary changes to the grabbed
     images so that the image is compatible with OpenCV operation.
 
     Args:
-        path (str): The path to the image if it reside on disk
-        stream (str): A stream of text representing where on the internet the image resides
+        path (str): The path to the image if it reside on disk.
+        stream (str): A stream of text representing where on the internet the image resides.
         url(str): Url representing a path to where an image should be fetched.
+        string(str): A base64 encoded string of the image.
 
     Raises:
             ValueError: If no path stream or URL was found.
@@ -47,6 +49,12 @@ def grab_image(path=None, stream=None, url=None):
             # If URL is incorrect Urllib in imutils library will catch the error.
             logger.debug("Downloading image from URL")
             return url_to_image(url)
+        # If string is not None, then the image was transmitted with base64 encoding.
+        elif string is not None:
+            logger.debug("Decoding base64 string")
+            encoded_data = string.split(',')[1]
+            nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         # if the stream is not None, then the image has been uploaded
         elif stream is not None:
             logger.debug("Downloading from image stream")
